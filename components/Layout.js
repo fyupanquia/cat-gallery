1b104e7c19113bb8f4cb4304972abc15c026ef58
fyupanquia <fyupanquia@outlook.com>
2019-10-12 15:47:06 -0500

    0.1

--- components/Layout.js
+++ components/Layout.js
@@ -16,7 +16,7 @@ export default class Layout extends React.Component {
 			<div className='cover-container d-flex w-100 h-100 p-3 mx-auto flex-column'>
 			  <header className='masthead mb-auto'>
 			    <div className='inner'>
-			      <h3 className='masthead-brand'>CatAlbum</h3>
+			      <h3 className='masthead-brand'>CatGallery</h3>
 			      <nav className='nav nav-masthead justify-content-center'>
 			        <a className='nav-link active' href='#'>Home</a>
 			        <a className='nav-link' href='#'>Features</a>
